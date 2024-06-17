961a9a8e8a0492f7078c1a1ed2035bfe10c341a7
Scott Lester <scott@redmaple.tech>
2024-06-17 09:43:50 +0100

    big tidy

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Overview
-A quick Go tool to grab the IPs for m365 services and print out a unique list, for calculating split tunnel configuration that doesn't push e.g. Teams down the VPN.
+A quick Go tool to grab the IPs for m365 services and print out a unique list, for calculating split tunnel VPN configuration that doesn't push e.g. Teams down the VPN.
 
-# Example for Wireguard
-We grab the data from, e.g. `https://endpoints.office.com/endpoints/worldwide?clientRequestId=e99b4f20-3062-4915-9c3a-17ae79e4ba65` (the request ID is just a client UUID).
+Uses the data published on [endpoints.office.com](https://endpoints.office.com/) - see [here](http://aka.ms/ipurlws) for documentation. Example data is in [m365_routes.json](./examples/m365_routes.json).
 
-This gives us [ms_routes.json](./ms_routes.json).
+# Running
+Our [main.go](main.go) grabs the data from the endpoint, unmarshals the JSON, and creates a map of all the unique IPs for all the entries. It them writes them to [ms_routes.txt](./examples/ms_routes.txt). You can filter with one of `Exchange`, `Skype`, `SharePoint` or `Common`. Leave blank for all. `Skype` means `Skype and Teams`.
 
-Our [main.go](main.go) grabs this, unmarshals the JSON, and creates a map of all the unique IPs for all the entries. And writes them to [ms_routes.txt](./ms_routes.txt).
+```shell
+go run main.go -f Common
+2024/06/17 09:37:18 Getting data from https://endpoints.office.com/endpoints/worldwide?clientRequestId=b9dd4f30-2d30-42c1-8ada-cc26221351e2
+2024/06/17 09:37:18 Parsing data
+        Microsoft 365 Common and Office Online 46: 20 IPs
+        Microsoft 365 Common and Office Online 56: 16 IPs
+        Microsoft 365 Common and Office Online 64: 5 IPs
+2024/06/17 09:37:18 Wrote addresses to "20240617_093718_m365_routes_Common.txt"
+2024/06/17 09:37:18 Fin.
+```
 
-You can add this to [Allowed IPs calculator](https://www.procustodibus.com/blog/2021/03/wireguard-allowedips-calculator/) to create an allow/blocklist:
+# Example with Wireguard
+First grab the data you want. E.g. to filter Teams calls from your VPN, run `go run main.go -f Skype`.
+
+You can add the data to [Allowed IPs calculator](https://www.procustodibus.com/blog/2021/03/wireguard-allowedips-calculator/) to create an allow/blocklist:
 
 ![WIREGUARD ALLOWEDIPS CALCULATOR](assets/2024-06-14-17-08-25.png)
 
-This gives us [allowed.txt](./allowed.txt).
+This gives us [allowed_teams_only.txt](./examples/allowed_teams_only.txt).
+
+Note trying to filter on all does come with a perfomance hit, presumably because of the huge routing table it creates!
+
--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/google/uuid"
 )
 
+const (
+	// see more: http://aka.ms/ipurlws
+	apiURL = "https://endpoints.office.com/endpoints/worldwide?clientRequestId="
+
+	all             = "all"
+	timestampFormat = "20060102_150405"
+)
+
+// thanks https://transform.tools/json-to-go
 type routeStruct []struct {
 	ID                     int      `json:"id"`
 	ServiceArea            string   `json:"serviceArea"`
@@ -28,78 +39,123 @@ type routeStruct []struct {
 }
 
 func main() {
+	var filter string
+	flag.StringVar(&filter, "f", all, "Service filter: 'Exchange', 'Skype' (inc. Teams), 'SharePoint', 'Common'. Leave blank for all services.")
+	flag.Parse()
+
+	if filter != all &&
+		filter != "Exchange" &&
+		filter != "Skype" &&
+		filter != "SharePoint" &&
+		filter != "Common" {
+		log.Printf("Unrecognised filter %q\n", filter)
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	// get the data from the endpoint
 	data, err := getData()
 
 	if err != nil {
 		log.Fatalf("Failed to get data: %s\n", err.Error())
 	}
 
+	// parse the received data
 	log.Println("Parsing data")
-	parseData(data)
+	err = parseData(data, filter)
+
+	if err != nil {
+		log.Fatalf("Failed to parse data: %q\n", err.Error())
+	}
 
 	log.Println("Fin.")
 }
 
 func getData() ([]byte, error) {
+	// prep the URL
 	uuid := uuid.New()
-	url := fmt.Sprintf("https://endpoints.office.com/endpoints/worldwide?clientRequestId=%s", uuid.String())
+	url := fmt.Sprintf("%s%s", apiURL, uuid.String())
 	log.Printf("Getting data from %s\n", url)
+
+	// get the data
 	resp, err := http.Get(url)
 
 	if err != nil {
 		return nil, err
 	}
 
+	// read the data from the response body
 	var content []byte
 	content, err = io.ReadAll(resp.Body)
 	resp.Body.Close()
 
+	if len(content) == 0 {
+		return nil, fmt.Errorf("failed to get any data")
+	}
+
 	return content, err
 }
 
-func parseData(data []byte) {
+func parseData(data []byte, filter string) error {
+	// unmarshal the JSON data
 	var parsed routeStruct
 	err := json.Unmarshal(data, &parsed)
 
 	if err != nil {
-		log.Fatalf("Failed to unmarshal JSON: %q\n", err.Error())
+		return fmt.Errorf("failed to unmarshal JSON: %q", err.Error())
 	}
 
+	// map to store all unique IPs
 	ips := make(map[string]bool)
 
+	// for all the routes in the struct
 	for _, route := range parsed {
+		// we don't care about entries without an IP
+		if len(route.Ips) > 0 {
+			{
+				// if we're including all or it matches the filter
+				if filter == all || strings.Contains(route.ServiceArea, filter) {
+					fmt.Printf("\t%s %d: %d IPs\n", route.ServiceAreaDisplayName, route.ID, len(route.Ips))
 
-		if len(route.Ips) > 0 && strings.Contains(route.ServiceArea, "Skype") {
-			fmt.Printf("\t%s: %d IPs\n", route.ServiceAreaDisplayName, len(route.Ips))
+					// for all the IPs it has
+					for _, i := range route.Ips {
 
-			// for all the IPs it has
-			for _, i := range route.Ips {
+						// add it to the map if we don't have it already
+						_, ok := ips[i]
 
-				// add it to the map if we don't have it already
-				_, ok := ips[i]
-
-				if !ok {
-					ips[i] = true
+						if !ok {
+							ips[i] = true
+						}
+					}
 				}
 			}
 		}
 	}
 
-	// write the IPs to file
-	filename := "ms_routes.txt"
+	err = writeToFile(ips, filter)
+
+	return err
+}
+
+func writeToFile(ips map[string]bool, filter string) error {
+	// create the file
+	filename := fmt.Sprintf("%s_m365_routes_%s.txt", time.Now().Format(timestampFormat), filter)
 	f, err := os.Create(filename)
 
 	if err != nil {
-		log.Fatalf("Failed to create file: %q\n", err.Error())
+		return fmt.Errorf("failed to create file %q: %s", filename, err.Error())
 	}
 
+	// write the IPs to file
 	w := bufio.NewWriter(f)
 
 	for ip := range ips {
 		w.WriteString(fmt.Sprintf("%s, ", ip))
 	}
 
+	// tidy up
 	w.Flush()
 	f.Close()
 	log.Printf("Wrote addresses to %q\n", filename)
+	return nil
 }
